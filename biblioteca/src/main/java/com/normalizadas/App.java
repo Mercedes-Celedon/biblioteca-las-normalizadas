4a87e8156ba7ef6c1c4f399a77dac09b2e8b6fa5
LauraGDev <laura.gilcara@gmail.com>
2024-07-25 11:23:38 +0200

    separates showAll into its own class

--- biblioteca/src/main/java/com/normalizadas/App.java
+++ biblioteca/src/main/java/com/normalizadas/App.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
+import com.normalizadas.crud.AllBooks;
 
 public class App {
     public static Scanner scanner = new Scanner(System.in);
@@ -38,67 +39,11 @@ public class App {
          * se pregunta si quiere salir
          * tiene que enseñar una lista de lo que debe hacer (swtich)
          */
-        showAll();
+        AllBooks allBooks = new AllBooks();
+        allBooks.showAll();
+
     }
 
-    /* Function show all books - showAll */
-    public static void showAll() throws SQLException {
-        System.out.println("\n\nListado de Libros disponibles: " + 
-        "\n\nID | Título | Autor | Género | ISBN | Stock | Idioma" +
-        "\n-----------------------------------------------------------------------------------------------------------------------------------------------------------");
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT b.id, b.title, b.isbn, b.stock, l.language FROM books as b\n" + //
-                "JOIN languages as l ON b.id_language=l.id\n" +
-                "ORDER BY b.id ASC");
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            String title = rs.getString("title");
-            String authors = getInfo(id, "authors");
-            String genres = getInfo(id, "genres");
-            String isbn = rs.getString("isbn");
-            int stock = rs.getInt("stock");
-            String language = rs.getString("language");
-
-            System.out.println(id + " | " + title + " | " + authors + " | " + genres + " | " + isbn + " | " + stock + " | " + language + 
-            "\n-----------------------------------------------------------------------------------------------------------------------------------------------------------");
-        }
-        stmt.close();
-        rs.close();
-    }
-
-    public static String getInfo(int id, String dataType) throws SQLException {
-        String data = "";
-        String query = "";
-        String column = "";
-        switch (dataType) {
-            case "authors":
-                query = "SELECT name from authors\n" + //
-                        "JOIN books_authors ON books_authors.id_author = authors.id\n" +
-                        "WHERE books_authors.id_book = " + id;
-                column = "name";
-                break;
-            case "genres":
-                query = "SELECT genre from genres\n" + //
-                        "JOIN books_genres ON books_genres.id_genre = genres.id\n" +
-                        "WHERE books_genres.id_book = " + id;
-                column = "genre";
-                break;
-            default:
-                break;
-        }
-
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-
-        while (rs.next()) {
-            data += rs.getString(column) + ", ";
-        }
-
-        stmt.close();
-        rs.close();
-
-        return data.substring(0, data.length() - 2);
-    }
 
     /* Function search books by filters - searchBooks */
     public void searchBooks() {
---
+++ biblioteca/src/main/java/com/normalizadas/crud/AllBooks.java
@@ -0,0 +1,73 @@
+package com.normalizadas.crud;
+
+import com.normalizadas.dbConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AllBooks {
+
+    public static dbConnection connection = new dbConnection();
+    public static Connection conn = connection.getDbConnection();
+
+    public static void showAll() throws SQLException {
+        System.out.println("\n\nListado de Libros disponibles: " +
+                "\n\nID | Título | Autor | Género | ISBN | Stock | Idioma" +
+                "\n-----------------------------------------------------------------------------------------------------------------------------------------------------------");
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT b.id, b.title, b.isbn, b.stock, l.language FROM books as b\n" + //
+                "JOIN languages as l ON b.id_language=l.id\n" +
+                "ORDER BY b.id ASC");
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String title = rs.getString("title");
+            String authors = getInfo(id, "authors");
+            String genres = getInfo(id, "genres");
+            String isbn = rs.getString("isbn");
+            int stock = rs.getInt("stock");
+            String language = rs.getString("language");
+
+            System.out.println(id + " | " + title + " | " + authors + " | " + genres + " | " + isbn + " | " + stock + " | " + language +
+                    "\n-----------------------------------------------------------------------------------------------------------------------------------------------------------");
+        }
+        stmt.close();
+        rs.close();
+        connection.closeConnection(conn);
+    }
+
+    public static String getInfo(int id, String dataType) throws SQLException {
+        String data = "";
+        String query = "";
+        String column = "";
+        switch (dataType) {
+            case "authors":
+                query = "SELECT name from authors\n" + //
+                        "JOIN books_authors ON books_authors.id_author = authors.id\n" +
+                        "WHERE books_authors.id_book = " + id;
+                column = "name";
+                break;
+            case "genres":
+                query = "SELECT genre from genres\n" + //
+                        "JOIN books_genres ON books_genres.id_genre = genres.id\n" +
+                        "WHERE books_genres.id_book = " + id;
+                column = "genre";
+                break;
+            default:
+                break;
+        }
+
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+
+        while (rs.next()) {
+            data += rs.getString(column) + ", ";
+        }
+
+        stmt.close();
+        rs.close();
+
+        return data.substring(0, data.length() - 2);
+    }
+}
