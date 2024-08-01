60979124baee43051982383de9f21d886830ec86
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2024-08-01 06:29:58 -0600

    adding testing files

---
+++ biblioteca/src/test/java/com/normalizadas/controller/AuthorsControllerTest.java
@@ -0,0 +1,20 @@
+package com.normalizadas.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthorsControllerTest {
+    @Test
+    void testFindOrCreateAuthor() {
+
+    }
+
+    @Test
+    void testGetAuthorsByBook() {
+
+    }
+
+    @Test
+    void testUpdateAuthor() {
+
+    }
+}
---
+++ biblioteca/src/test/java/com/normalizadas/controller/GenresControllerTest.java
@@ -0,0 +1,20 @@
+package com.normalizadas.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class GenresControllerTest {
+    @Test
+    void testFindOrCreateGenre() {
+
+    }
+
+    @Test
+    void testGetGenresByBook() {
+
+    }
+
+    @Test
+    void testUpdateGenre() {
+
+    }
+}
